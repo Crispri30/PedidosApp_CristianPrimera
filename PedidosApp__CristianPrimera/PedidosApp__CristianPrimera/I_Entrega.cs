f525e38dc9995d3913a70677a39633b9bb60beff
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-04-29 13:49:36 -0500

    Agregar Interfaz de entrega

---
+++ PedidosApp__CristianPrimera/PedidosApp__CristianPrimera/I_Entrega.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp__CristianPrimera
+{
+    public class I_Entrega
+    {
+       public interface IMetodoEntrega
+        {
+            double CalcularCosto(int km);
+            string TipoEntrega();
+        }
+    }
+}
