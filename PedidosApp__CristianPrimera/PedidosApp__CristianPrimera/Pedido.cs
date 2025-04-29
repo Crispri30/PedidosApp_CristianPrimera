023bb3e38306b1b3a890903cabc5da435b2f2ee9
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-04-29 13:58:33 -0500

    Agregar clase pedido

---
+++ PedidosApp__CristianPrimera/PedidosApp__CristianPrimera/Pedido.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PedidosApp__CristianPrimera.I_Entrega;
+
+namespace PedidosApp__CristianPrimera
+{
+    public class Pedido
+    {
+        public string Cliente { get; set; }
+        public string Producto { get; set; }
+        public bool Urgente { get; set; }
+        public double Peso { get; set; }
+        public int Distancia { get; set; }
+        public IMetodoEntrega MetodoEntrega { get; set; }
+        public Pedido(string cliente, string producto, bool urgente, double peso, int distancia)
+        {
+            Cliente = cliente;
+            Producto = producto;
+            Urgente = urgente;
+            Peso = peso;
+            Distancia = distancia;
+            MetodoEntrega = FactoryMethod.CrearEntrega(producto, urgente, peso);
+        }
+        public double ObtenerCosto() => MetodoEntrega.CalcularCosto(Distancia);
+    }
+}
