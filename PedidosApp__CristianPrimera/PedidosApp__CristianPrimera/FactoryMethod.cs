0bc747cd135dbfdd7a8f542358f0c68f7f8f0b15
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-04-29 13:57:00 -0500

    Agregar factory method

---
+++ PedidosApp__CristianPrimera/PedidosApp__CristianPrimera/FactoryMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PedidosApp__CristianPrimera.EstrategiasEntrega;
+using static PedidosApp__CristianPrimera.I_Entrega;
+
+namespace PedidosApp__CristianPrimera
+{
+    public class FactoryMethod
+    {
+            public static IMetodoEntrega CrearEntrega(string tipoProducto, bool urgente, double
+            peso)
+            {
+                if (tipoProducto == "tecnología" && urgente)
+                    return new EntregaDron();
+                else if (tipoProducto == "accesorio")
+                    return new EntregaMoto();
+                else if (tipoProducto == "componente" || peso > 10)
+                    return new EntregaCamion();
+                else
+                    return new EntregaMoto(); // valor por defecto
+            }
+    }
+}
