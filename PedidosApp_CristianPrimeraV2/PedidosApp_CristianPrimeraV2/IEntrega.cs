43db2494a6498cc1e47fb706bb3492811a075983
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-04-29 19:27:04 -0500

    Agregar interfaz de entrega V2 versión de .net actualizada

---
+++ PedidosApp_CristianPrimeraV2/PedidosApp_CristianPrimeraV2/IEntrega.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp_CristianPrimeraV2
+{
+    public class IEntrega
+    {
+        public interface IMetodoEntrega
+        {
+            double CalcularCosto(int km);
+            string TipoEntrega();
+        }
+    }
+}
