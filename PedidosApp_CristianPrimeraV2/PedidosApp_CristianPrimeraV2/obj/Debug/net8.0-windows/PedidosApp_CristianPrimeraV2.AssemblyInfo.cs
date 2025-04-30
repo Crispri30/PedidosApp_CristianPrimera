cfc502eb34e4af07201553204a8a343ff4315930
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-04-29 19:34:03 -0500

    Agregar clase pedido V2 versión .net actualizada

--- PedidosApp_CristianPrimeraV2/PedidosApp_CristianPrimeraV2/obj/Debug/net8.0-windows/PedidosApp_CristianPrimeraV2.AssemblyInfo.cs
+++ PedidosApp_CristianPrimeraV2/PedidosApp_CristianPrimeraV2/obj/Debug/net8.0-windows/PedidosApp_CristianPrimeraV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PedidosApp_CristianPrimeraV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2230c4c5204985e15512eca6054e5efa3163ce51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de73b5876fea6ccf2c498ed87260af696c33727")]
 [assembly: System.Reflection.AssemblyProductAttribute("PedidosApp_CristianPrimeraV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PedidosApp_CristianPrimeraV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
