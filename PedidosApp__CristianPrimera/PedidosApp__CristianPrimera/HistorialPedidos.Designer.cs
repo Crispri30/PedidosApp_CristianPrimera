a92ed5777c811d7ad4bcd3f9d4b29aa913af914b
Cristian Primera <cristian.primera789@pascualbravo.edu.co>
2025-04-29 21:35:44 -0500

    Agregar archivos del proyecto

--- PedidosApp__CristianPrimera/PedidosApp__CristianPrimera/HistorialPedidos.Designer.cs
+++ PedidosApp__CristianPrimera/PedidosApp__CristianPrimera/HistorialPedidos.Designer.cs
@@ -30,15 +30,16 @@
         {
             this.dgvHistorial = new System.Windows.Forms.DataGridView();
             this.cmbFiltro = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvHistorial
             // 
             this.dgvHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHistorial.Location = new System.Drawing.Point(161, 63);
+            this.dgvHistorial.Location = new System.Drawing.Point(100, 40);
             this.dgvHistorial.Name = "dgvHistorial";
-            this.dgvHistorial.Size = new System.Drawing.Size(461, 296);
+            this.dgvHistorial.Size = new System.Drawing.Size(529, 324);
             this.dgvHistorial.TabIndex = 0;
             // 
             // cmbFiltro
@@ -49,17 +50,28 @@
             this.cmbFiltro.Size = new System.Drawing.Size(121, 21);
             this.cmbFiltro.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(664, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filtro tipo de entrega";
+            // 
             // HistorialPedidos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cmbFiltro);
             this.Controls.Add(this.dgvHistorial);
             this.Name = "HistorialPedidos";
             this.Text = "HistorialPedidos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,5 +79,6 @@
 
         private System.Windows.Forms.DataGridView dgvHistorial;
         private System.Windows.Forms.ComboBox cmbFiltro;
+        private System.Windows.Forms.Label label1;
     }
 }
